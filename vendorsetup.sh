d398a0592fcac62dbfa3e04561b4451b4486bd50
Adeel Javed <alwaysadeel@gmail.com>
2015-05-06 16:14:29 +0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-add_lunch_combo cm_pepper-user
-add_lunch_combo cm_pepper-userdebug
-add_lunch_combo cm_pepper-eng
+add_lunch_combo full_pepper-user
+add_lunch_combo full_pepper-userdebug
+add_lunch_combo full_pepper-eng
 
